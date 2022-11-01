b3457832d12739fea85312659f79776d57234702
vbsouza20 <116775310+vbsouza20@users.noreply.github.com>
2022-11-01 01:08:24 -0300

    Revert "Revert "Update app.js""
    This reverts commit b203058d8d879ca6a53a2202180abcc9c25353b4.

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+<<<<<<< HEAD
 console.log("Aplicação está se conectando ao banco de dados")
+// console teste:
+console.log ("commit teste aula")
+=======
+>>>>>>> parent of cc841af (atualizando init projeto)
